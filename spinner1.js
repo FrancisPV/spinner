bd8f91f61c0cc1e68a3043c79c2b7bc32f9bb48f
Françis <francispellerinvenne@hotmail.fr>
2020-06-30 16:41:45 +0000

    Spinner

---
+++ spinner1.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 150);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 450);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 600);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 750);
+
+setTimeout(() => {
+  process.stdout.write('\r/  '); 
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r-   '); 
+}, 1050);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 1200);
+
+setTimeout(() => {
+  process.stdout.write('\r|   '); 
+}, 1350);
